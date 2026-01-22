7ab40d9b3c0e53bbf9ab4b45495cc10d67dd389f
Ghanshyam <ghanshyampatil2002@gmail.com>
2026-01-23 00:00:39 +0530

    '4-commit'

--- setup.py
+++ setup.py
@@ -1,40 +1,38 @@
 '''
-The setup.py file is an essential part of packaging and
-distributing Python projects. It is used by setuptools
-(or distutils in older Python versions) to define the configuration
+The setup.py file is an essential part of packaging and 
+distributing Python projects. It is used by setuptools 
+(or distutils in older Python versions) to define the configuration 
 of your project, such as its metadata, dependencies, and more
 '''
 
-from setuptools import find_packages, setup
+from setuptools import find_packages,setup
 from typing import List
 
-
-def get_requirements() -> List[str]:
+def get_requirements()->List[str]:
     """
     Thiss function will return list of requirements
-
+    
     """
-    requirement_lst: List[str] = []
+    requirement_lst:List[str]=[]
     try:
-        with open('requirements.txt', 'r') as file:
-            # Read lines from the file
-            lines = file.readlines()
+        with open('requirements.txt','r') as file:
+            #Read lines from the file
+            lines=file.readlines()
             ## Process each line
             for line in lines:
-                requirement = line.strip()
+                requirement=line.strip()
                 ## ignore empty lines and -e .
-                if requirement and requirement != '-e .':
+                if requirement and requirement!= '-e .':
                     requirement_lst.append(requirement)
     except FileNotFoundError:
         print("requirements.txt file not found")
 
     return requirement_lst
 
-
 setup(
     name="NetworkSecurity",
     version="0.0.3",
-    author="Ghansyam Patil",
+    author="Ghanshyam",
     author_email="ghanshyampatil2002@gmail.com",
     packages=find_packages(),
     install_requires=get_requirements()
